9291ca0e5c250bac31b4497c996c39c70abfbe6b
Md Emon Bhuiyan <mdemonbhuiyan@live.com>
2025-07-15 19:47:04 +0300

    Update seo.js

--- src/components/common/seo.js
+++ src/components/common/seo.js
@@ -27,18 +27,6 @@ function SEO({ description, lang, meta, title }) {
 
   const metaDescription = description || site.siteMetadata.description
 
-  // Define your GTM ID here. For production, consider using environment variables.
-  const gtmId = 'GTM-W5VV2CXJ';
-
-  // The GTM script code as a string
-  const gtmScript = `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','${gtmId}');
-  `;
-
   return (
     <Helmet
       htmlAttributes={{
